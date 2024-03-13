b1c09d5f4c606463de2ee5f99e22912629f743ff
sigwyg <sigwyg@users.noreply.github.com>
2024-03-13 18:04:48 +0900

    freshお試しWIP

--- todo-data-storage/frontend/fresh/routes/index.tsx
+++ todo-data-storage/frontend/fresh/routes/index.tsx
@@ -1,25 +1,37 @@
 import { useSignal } from "@preact/signals";
-import Counter from "../islands/Counter.tsx";
+import { Handlers, PageProps } from "$fresh/server.ts";
 
-export default function Home() {
+const NAMES = ["Alice", "Bob", "Charlie", "Dave", "Eve", "Frank"];
+
+type ToDo = {
+  id: string;
+  title: string;
+  completed: boolean;
+};
+
+export const handler: Handlers<ToDo[] | null> = {
+  async GET(req, _ctx) {
+    console.log(req);
+    const todos = (await req.json()) as ToDo[];
+    return new Response(JSON.stringify(todos));
+  },
+};
+
+export default function Home({ data }: PageProps<Data>) {
+  const { results, query } = data;
   const count = useSignal(3);
   return (
     <div class="px-4 py-8 mx-auto bg-[#86efac]">
-      <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center">
-        <img
-          class="my-6"
-          src="/logo.svg"
-          width="128"
-          height="128"
-          alt="the Fresh logo: a sliced lemon dripping with juice"
+      <h1>Todo Data Storage</h1>
+      <form>
+        <input
+          type="text"
+          name="title"
+          placeholder="What needs to be done?"
+          value={query}
         />
-        <h1 class="text-4xl font-bold">Welcome to Fresh</h1>
-        <p class="my-4">
-          Try updating this message in the
-          <code class="mx-2">./routes/index.tsx</code> file, and refresh.
-        </p>
-        <Counter count={count} />
-      </div>
+        <button type="submit">Add</button>
+      </form>
     </div>
   );
 }
